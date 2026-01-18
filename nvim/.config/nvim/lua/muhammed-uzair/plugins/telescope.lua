726181e511650e2a0089cf04f0d6730c8e126236
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-01-18 20:18:40 +0400

    feat(lazy.nvim): add telescope plugin

---
+++ nvim/.config/nvim/lua/muhammed-uzair/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+
+    telescope.setup({
+      defaults = {
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+          },
+        },
+      },
+    })
+
+    -- keymaps
+    vim.keymap.set("n", "<leader>lg", builtin.live_grep, {
+      desc = "Telescope Live Grep",
+    })
+
+    vim.keymap.set("n", "<C-p>", builtin.find_files, {
+      desc = "Telescope Find Files",
+    })
+  end,
+}
