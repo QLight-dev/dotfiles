911613a74e77b6c02a289c8fd38f52fd038db667
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-01-25 18:45:40 +0400

    fix(telescope.nvim): fix treesitter error

--- nvim/.config/nvim/lua/muhammed-uzair/plugins/telescope.lua
+++ nvim/.config/nvim/lua/muhammed-uzair/plugins/telescope.lua
@@ -1,31 +1,33 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.6",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local builtin = require("telescope.builtin")
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
 
-    telescope.setup({
-      defaults = {
-        sorting_strategy = "ascending",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				sorting_strategy = "ascending",
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+					},
+				},
+				preview = {
+					treesitter = false,
+				},
+			},
+		})
 
-    -- keymaps
-    vim.keymap.set("n", "<leader>lg", builtin.live_grep, {
-      desc = "Telescope Live Grep",
-    })
+		-- keymaps
+		vim.keymap.set("n", "<leader>lg", builtin.live_grep, {
+			desc = "Telescope Live Grep",
+		})
 
-    vim.keymap.set("n", "<C-p>", builtin.find_files, {
-      desc = "Telescope Find Files",
-    })
-  end,
+		vim.keymap.set("n", "<C-p>", builtin.find_files, {
+			desc = "Telescope Find Files",
+		})
+	end,
 }
