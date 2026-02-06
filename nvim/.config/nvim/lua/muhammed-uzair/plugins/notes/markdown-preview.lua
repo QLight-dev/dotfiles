e55744006d389db211b28bacba50167d621aecd0
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-02-06 15:37:40 +0400

    fix(markdown-preview): fix always local changes happening

--- nvim/.config/nvim/lua/muhammed-uzair/plugins/notes/markdown-preview.lua
+++ nvim/.config/nvim/lua/muhammed-uzair/plugins/notes/markdown-preview.lua
@@ -3,4 +3,7 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = "cd app && npm install",  -- run npm install instead of calling the Vim function
+    git = {
+            ignore = {"app/yarn.lock"},
+    }
 }
