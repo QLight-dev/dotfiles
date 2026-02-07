1360bf7d07de49a1fa02e77269a35627de720652
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-02-07 15:13:14 +0400

    chore(scripts): add daily-maintanance.zsh script

---
+++ scripts/.local/bin/daily-maintanance.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+clear
+
+# update packages
+echo "updating packages..."
+sudo pacman -Syyu --noconfirm
+
+# update nvim
+echo "update neovim..."
+cd ~/dotfiles
+nvim --headless +Lazy! sync +qa
+
+echo "commiting changes..."
+git add nvim/.config/nvim/lazy-lock.json
+git commit -m "chore(lazy.nvim): update plugins"
+git push
+
+# update oh my zsh
+echo "updating oh-my-zsh"
+$ZSH/tools/upgrade.sh
+
+echo "done"
