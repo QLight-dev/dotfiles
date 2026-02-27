d9aeb7d3570cfbc20e88b0df08bcbcd2779ccd24
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-02-27 14:54:25 +0400

    chore(treesitter.nvim): remove treesitter context

--- nvim/.config/nvim/lua/muhammed-uzair/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/muhammed-uzair/plugins/treesitter.lua
@@ -1,21 +1,20 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    opts = {
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        -- coding
-        "go",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		lazy = false,
+		opts = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = {
+				-- coding
+				"go",
 
-        -- config
-        "lua", -- nvim
-        "toml", -- starship
-        "zsh" -- shell and scripts
-      },
-    },
-  },
+				-- config
+				"lua", -- nvim
+				"toml", -- starship
+				"zsh", -- shell and scripts
+			},
+		},
+	},
 }
-
