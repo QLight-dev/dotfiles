b4edc28cac4ecf4bd0ddb6fa4040aac20a5c5cbf
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-05 15:28:05 +0400

    feat(codecompanion.nvim): added support for smaller model

--- nvim/.config/nvim/lua/muhammed-uzair/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/muhammed-uzair/plugins/codecompanion.lua
@@ -1,28 +1,46 @@
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    strategies = {
-      chat = { adapter = "groq" },
-      inline = { adapter = "groq" },
-    },
-    adapters = {
-      groq = function()
-        return require("codecompanion.adapters").extend("openai", {
-          env = {
-            api_key = os.getenv("GROQ_API_KEY"), -- Export key in shell
-            url = "https://api.groq.com/openai/v1", -- Correct base URL
-          },
-          schema = {
-            model = {
-              default = "llama-3.3-70b-versatile", -- Recommended model
-            },
-          },
-        })
-      end,
-    },
-  },
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+
+	opts = {
+		strategies = {
+			chat = { adapter = "groq_big" },
+			inline = { adapter = "groq_small" },
+		},
+
+		adapters = {
+			http = {
+				groq_big = function()
+					return require("codecompanion.adapters").extend("openai_compatible", {
+						env = {
+							api_key = "GROQ_API_KEY",
+							url = "https://api.groq.com/openai",
+						},
+						schema = {
+							model = {
+								default = "llama-3.3-70b-versatile",
+							},
+						},
+					})
+				end,
+
+				groq_small = function()
+					return require("codecompanion.adapters").extend("openai_compatible", {
+						env = {
+							api_key = "GROQ_API_KEY",
+							url = "https://api.groq.com/openai",
+						},
+						schema = {
+							model = {
+								default = "llama-3.1-8b-instant",
+							},
+						},
+					})
+				end,
+			},
+		},
+	},
 }
