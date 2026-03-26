e26d4ec5a2f09d522cb1ebf1fd85ac3bc9933d1e
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-26 18:42:56 +0400

    refactor(codecompanion): switch to gpt_oss adapters and strategies

--- nvim/.config/nvim/lua/muhammed-uzair/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/muhammed-uzair/plugins/codecompanion.lua
@@ -7,28 +7,39 @@ return {
 
 	opts = {
 		strategies = {
-			chat = { adapter = "groq" },
-			inline = { adapter = "groq" },
+			chat = { adapter = "gpt_oss_120b" },
+			inline = { adapter = "gpt_oss_20b" },
 		},
-
 		adapters = {
-			groq = function()
-				return require("codecompanion.adapters").extend("openai_compatible", {
-					env = {
-						api_key = "GROQ_API_KEY",
-						url = "https://api.groq.com/openai",
-					},
-					schema = {
-						model = {
-							default = "qwen/qwen3-32b",
-							choices = {
-								"openai/gpt-oss-120b",
-								"qwen/qwen3-32b",
+			http = {
+				gpt_oss_120b = function()
+					return require("codecompanion.adapters").extend("openai_compatible", {
+						env = {
+							api_key = "GROQ_API_KEY",
+							url = "https://api.groq.com/openai",
+						},
+						schema = {
+							model = {
+								default = "openai/gpt-oss-120b",
 							},
 						},
-					},
-				})
-			end,
+					})
+				end,
+
+				gpt_oss_20b = function()
+					return require("codecompanion.adapters").extend("openai_compatible", {
+						env = {
+							api_key = "GROQ_API_KEY",
+							url = "https://api.groq.com/openai",
+						},
+						schema = {
+							model = {
+								default = "openai/gpt-oss-20b",
+							},
+						},
+					})
+				end,
+			},
 		},
 	},
 }
