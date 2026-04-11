de4372b880767df1c75607fbfd6197f683ada76a
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-04-11 19:23:19 +0400

    feat(core): set shiftwidth to 2 in options.lua

--- nvim/.config/nvim/lua/muhammed-uzair/core/options.lua
+++ nvim/.config/nvim/lua/muhammed-uzair/core/options.lua
@@ -7,6 +7,7 @@ vim.opt.relativenumber = true
 -- a few options
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
